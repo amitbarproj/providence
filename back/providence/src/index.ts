86b38b35d0814bab6d517606c5dba07d8e701d56
amitbarproj <amitbarproj@gmail.com>
2023-04-06 12:38:17 +0300

    df

---
+++ back/providence/src/index.ts
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const https = require("https");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+const projConf = require("../../../config/projConf.json");
+const port  = projConf.ProvidenceServer.port;
+const server = projConf.ProvidenceServer.protocol === "https" ? https.createServer(app) :http.createServer(app);
+
+
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET","POST"]
+    }
+});
+
+io.on("connection" , (socket) => {
+    console.log(`User Connected: ${socket}`);
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+
+
+server.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
