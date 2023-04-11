b2f611e54731a615eb7f30468a81168b221a32ee
amitbarproj <amitbarproj@gmail.com>
2023-04-11 15:49:26 +0300

    dgd

--- back/providence/src/services/game.ts
+++ back/providence/src/services/game.ts
@@ -1,3 +1,4 @@
+import e = require("cors");
 import { Player } from "./player";
 import { SocketServer } from "./socketServer";
 
@@ -5,17 +6,38 @@ export class Game {
     private roomId: string = undefined
     private players: Map<string, Player> = undefined;
     private minPlayers: number = undefined
+    private myIterator = undefined;
+    private currentPlayer = undefined
 
     constructor(players: Map<string, Player> , roomId: string, minPlayers: number ) {
         this.players = players;
+        this.myIterator = this.players.entries();
+        this.currentPlayer = this.myIterator.next();
         this.roomId = roomId;
         this.minPlayers = minPlayers;
         setInterval(() => {
-            SocketServer.sendGameMessage(this.roomId , this.players.size ); 
-        }, 1000)
+             this.setNextPlayer();   
+        }, 3000)
+
+        setInterval(() => {
+            console.log(this.currentPlayer.value);;   
+       }, 500)
+    }
+
+
+
+    private setNextPlayer = () => {
+       
+        this.currentPlayer = this.myIterator.next();
+        if(this.currentPlayer.done) {
+            this.myIterator = this.players.entries();
+            this.currentPlayer = this.myIterator.next();
+        }
+
     }
 
 
 
 }
 
+        // SocketServer.sendGameMessage(this.roomId , this.currentPlayer.value );
