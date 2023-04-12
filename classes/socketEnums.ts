1caaf8fa97a835e46d6983478b9f45a747f67280
amitbarproj <amitbarproj@gmail.com>
2023-04-12 20:14:42 +0300

    j

--- classes/socketEnums.ts
+++ classes/socketEnums.ts
@@ -1,4 +1,3 @@
 export enum SOCKET_ENUMS  {
     YOU_ARE_NEW_ADMIN = "YOU_ARE_NEW_ADMIN",
-  
 }
--- front/src/Components/CreateRoomModal/CreateRoomModal.js
+++ front/src/Components/CreateRoomModal/CreateRoomModal.js
@@ -1,31 +1,32 @@
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { useState, useEffect, useRef } from "react";
+import Collapse from "react-bootstrap/Collapse";
+import Col from "react-bootstrap/Col";
 
+import { useState, useRef } from "react";
 
 const CreateRoomModal = (props) => {
   const createRoomError = props.createRoomError;
   const createRoomCallback = props.createRoomCallback;
   const newRoomId = useRef();
-  const newAuth = useRef();
   const newSecret = useRef();
   const newUsername = useRef();
-  const newMaxPlayers = useRef();
-
+  const [open, setOpen] = useState(false);
+  const [newMaxPlayers, setNewMaxPlayers] = useState(9);
 
   const createRoom = () => {
     const dataToSend = {
-        roomId: newRoomId.current.value,
-        auth: newAuth.current.checked,
-        secret: newSecret.current.value,
-        username: newUsername.current.value,
-        maxPlayers: Number(newMaxPlayers.current.value),
-        minPlayers: undefined,
-      };
-      createRoomCallback(dataToSend);
-  }  
-
+      roomId: newRoomId.current.value,
+      auth: open,
+      secret: newSecret.current.value,
+      username: newUsername.current.value,
+      maxPlayers: newMaxPlayers,
+      minPlayers: undefined,
+    };
+    createRoomCallback(dataToSend);
+    console.log(dataToSend);
+  };
 
   return (
     <Modal
@@ -41,13 +42,48 @@ const CreateRoomModal = (props) => {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Please Enter:</h4>
-        <input placeholder="Room ID" ref={newRoomId}></input>
-        <input placeholder="Username" ref={newUsername}></input>
-        <input placeholder="Room Secret" ref={newSecret}></input>
-        Max PLayers:
-        <Form.Range ref={newMaxPlayers} min={3} max={9} />
-        <Form.Switch ref={newAuth} />
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Username"
+            ref={newUsername}
+          />
+          <Form.Label>Room ID</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Room ID"
+            ref={newRoomId}
+          />
+          <Form.Label>Maximum Players: {newMaxPlayers}</Form.Label>
+          <Form.Range
+            value={newMaxPlayers}
+            onChange={(e) => {
+              setNewMaxPlayers(Number(e.target.value));
+            }}
+            min={3}
+            max={9}
+          />
+          <Form.Switch
+            onClick={() => setOpen(!open)}
+            //   aria-controls="example-collapse-text"
+            //   aria-expanded={open}
+            checked={open}
+          />
+          <Collapse in={open}>
+            <div id="example-collapse-text">
+              <Form.Label>Room Secret</Form.Label>
+              <Form.Control
+                required
+                type="password"
+                placeholder="Secret"
+                ref={newSecret}
+              />
+            </div>
+          </Collapse>
+        </Form.Group>
       </Modal.Body>
       <p>{createRoomError}</p>
       <Modal.Footer>
