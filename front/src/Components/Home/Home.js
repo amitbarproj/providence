6b597ebcae0560c82906b200e393be853b027ab5
amitbarproj <amitbarproj@gmail.com>
2023-05-18 16:53:00 +0300

    ghfj

--- front/src/Components/Home/Home.js
+++ front/src/Components/Home/Home.js
@@ -1,7 +1,19 @@
 import { LOCAL_STORAGE, SERVER_URL } from "../../Enums/enums";
 import { useState, useEffect, useRef, forwardRef } from "react";
 import { useNavigate } from "react-router-dom";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import InfoIcon from "@mui/icons-material/Info";
+import Popover from "@mui/material/Popover";
+import WavingHandIcon from "@mui/icons-material/WavingHand";
+import { Divider } from "@mui/material";
+import { BACKGROUNDS, PLAYERS_BACKGROUND_COLOR } from "../../Enums/enums";
 
 const serverURL = `${SERVER_URL.protocol}://${SERVER_URL.host}:${SERVER_URL.port}`;
 
@@ -12,14 +24,30 @@ const Home = () => {
 
   return (
     <div className="home">
-      <h1>HOME</h1>
-      <Button
-        onClick={() => {
-          navigate(`/rooms`);
-        }}
-      >
-        Go to rooms
-      </Button>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar
+          position="static"
+          style={{ backgroundImage: BACKGROUNDS.AppBar }}
+        >
+          <Toolbar>
+            <IconButton
+              onClick={() => {
+                navigate(`/rooms`);
+              }}
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <SportsEsportsIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Multiplayer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </div>
   );
 };
